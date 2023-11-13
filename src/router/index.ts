c25599a33e3289fc9b1b5c1d2c5cc566c29f71a8
nekoi <nekoi275@gmail.com>
2023-11-13 17:52:37 +0200

    router refactoring

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,103 +14,56 @@ function getData(apiStore: any, tableName: String, callback: (response: any) => 
     .catch((error: any) => console.error(error))
 }
 
+interface Header {
+  name: string
+  displayName: string
+  isFilter: boolean
+}
+
+const headers: { [name: string]: Array<Header> } = {
+  clothes: [
+    { name: 'type', displayName: 'TYPE', isFilter: true },
+    { name: 'color', displayName: 'COLOR', isFilter: true },
+    { name: 'description', displayName: 'DESCRIPTION', isFilter: false },
+    { name: 'price', displayName: 'PRICE', isFilter: false },
+    { name: 'year', displayName: 'YEAR', isFilter: true },
+    { name: 'season', displayName: 'SEASON', isFilter: true }
+  ],
+  accessories: [
+    { name: 'type', displayName: 'TYPE', isFilter: true },
+    { name: 'color', displayName: 'COLOR', isFilter: true },
+    { name: 'description', displayName: 'DESCRIPTION', isFilter: false },
+    { name: 'price', displayName: 'PRICE', isFilter: false },
+    { name: 'year', displayName: 'YEAR', isFilter: true },
+    { name: '', displayName: '', isFilter: false }
+  ],
+  old: [
+    { name: 'type', displayName: 'TYPE', isFilter: true },
+    { name: 'color', displayName: 'COLOR', isFilter: true },
+    { name: 'description', displayName: 'DESCRIPTION', isFilter: false },
+    { name: 'price', displayName: 'PRICE', isFilter: false },
+    { name: 'year', displayName: 'YEAR', isFilter: true },
+    { name: '', displayName: '', isFilter: false }
+  ]
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/clothes',
       name: 'clothes',
-      component: ClothesView,
-      beforeEnter: () => {
-        const api = useApiStore()
-        const tableStore = useTableStore()
-        const sidebarStore = useSidebarStore()
-        tableStore.updateHeaders([
-          { name: 'type', displayName: 'TYPE', isFilter: true },
-          { name: 'color', displayName: 'COLOR', isFilter: true },
-          { name: 'description', displayName: 'DESCRIPTION', isFilter: false },
-          { name: 'price', displayName: 'PRICE', isFilter: false },
-          { name: 'year', displayName: 'YEAR', isFilter: true },
-          { name: 'season', displayName: 'SEASON', isFilter: true }
-        ])
-        if (tableStore.clothes.length == 0) {
-          getData(api, 'clothes', (response: any) => {
-            tableStore.clothes = [...response]
-            tableStore.current.rows = [...tableStore.clothes.filter((item) => !item.isOld)]
-            sidebarStore.getFilters(tableStore.current)
-          })
-        } else {
-          tableStore.current.rows = [...tableStore.clothes.filter((item) => !item.isOld)]
-          sidebarStore.getFilters(tableStore.current)
-        }
-      }
+      component: ClothesView
     },
     {
       path: '/accessories',
       name: 'accessories',
-      component: AccessoriesView,
-      beforeEnter: () => {
-        const api = useApiStore()
-        const tableStore = useTableStore()
-        const sidebarStore = useSidebarStore()
-        tableStore.updateHeaders([
-          { name: 'type', displayName: 'TYPE', isFilter: true },
-          { name: 'color', displayName: 'COLOR', isFilter: true },
-          { name: 'description', displayName: 'DESCRIPTION', isFilter: false },
-          { name: 'price', displayName: 'PRICE', isFilter: false },
-          { name: 'year', displayName: 'YEAR', isFilter: true },
-          { name: '', displayName: '', isFilter: false }
-        ])
-        if (tableStore.accessories.length == 0) {
-          getData(api, 'accessories', (response: any) => {
-            tableStore.accessories = [...response]
-            tableStore.current.rows = [...tableStore.accessories.filter((item) => !item.isOld)]
-            sidebarStore.getFilters(tableStore.current)
-          })
-        } else {
-          tableStore.current.rows = [...tableStore.accessories.filter((item) => !item.isOld)]
-          sidebarStore.getFilters(tableStore.current)
-        }
-      }
+      component: AccessoriesView
     },
     {
       path: '/old',
       name: 'old',
-      component: OldView,
-      beforeEnter: () => {
-        const api = useApiStore()
-        const tableStore = useTableStore()
-        const sidebarStore = useSidebarStore()
-        tableStore.updateHeaders([
-          { name: 'type', displayName: 'TYPE', isFilter: true },
-          { name: 'color', displayName: 'COLOR', isFilter: true },
-          { name: 'description', displayName: 'DESCRIPTION', isFilter: false },
-          { name: 'price', displayName: 'PRICE', isFilter: false },
-          { name: 'year', displayName: 'YEAR', isFilter: true },
-          { name: 'season', displayName: 'SEASON', isFilter: true }
-        ])
-        tableStore.current.rows = []
-        if (tableStore.clothes.length == 0) {
-          getData(api, 'clothes', (response: any) => {
-            tableStore.clothes = [...response]
-            tableStore.current.rows.push(...tableStore.clothes.filter((item) => item.isOld))
-            sidebarStore.getFilters(tableStore.current)
-          })
-        } else {
-          tableStore.current.rows.push(...tableStore.clothes.filter((item) => item.isOld))
-          sidebarStore.getFilters(tableStore.current)
-        }
-        if (tableStore.accessories.length == 0) {
-          getData(api, 'accessories', (response: any) => {
-            tableStore.accessories = [...response]
-            tableStore.current.rows.push(...tableStore.accessories.filter((item) => item.isOld))
-            sidebarStore.getFilters(tableStore.current)
-          })
-        } else {
-          tableStore.current.rows.push(...tableStore.accessories.filter((item) => item.isOld))
-          sidebarStore.getFilters(tableStore.current)
-        }
-      }
+      component: OldView
     },
     {
       path: '/moodboard',
@@ -120,4 +73,53 @@ const router = createRouter({
   ]
 })
 
+router.beforeEach((to) => {
+  const api = useApiStore()
+  const tableStore = useTableStore()
+  const sidebarStore = useSidebarStore()
+  if (to.name === 'clothes' || to.name === 'accessories') {
+    const tableName = to.name
+    tableStore.updateHeaders(headers[tableName])
+    if (tableStore[tableName].length == 0) {
+      getData(api, tableName, (response: any) => {
+        tableStore[tableName] = [...response]
+        tableStore.current.rows = [...tableStore[tableName].filter((item) => !item.isOld)]
+        sidebarStore.getFilters(tableStore.current)
+        tableStore.filtered = tableStore.current.rows
+      })
+    } else {
+      tableStore.current.rows = [...tableStore[tableName].filter((item) => !item.isOld)]
+      sidebarStore.getFilters(tableStore.current)
+      tableStore.filtered = tableStore.current.rows
+    }
+  }
+  if (to.name === 'old') {
+    tableStore.updateHeaders(headers.old)
+    tableStore.current.rows = []
+    if (tableStore.clothes.length == 0) {
+      getData(api, 'clothes', (response: any) => {
+        tableStore.clothes = [...response]
+        tableStore.current.rows.push(...tableStore.clothes.filter((item) => item.isOld))
+        sidebarStore.getFilters(tableStore.current)
+        tableStore.filtered = tableStore.current.rows
+      })
+    } else {
+      tableStore.current.rows.push(...tableStore.clothes.filter((item) => item.isOld))
+      sidebarStore.getFilters(tableStore.current)
+      tableStore.filtered = tableStore.current.rows
+    }
+    if (tableStore.accessories.length == 0) {
+      getData(api, 'accessories', (response: any) => {
+        tableStore.accessories = [...response]
+        tableStore.current.rows.push(...tableStore.accessories.filter((item) => item.isOld))
+        sidebarStore.getFilters(tableStore.current)
+        tableStore.filtered = tableStore.current.rows
+      })
+    } else {
+      tableStore.current.rows.push(...tableStore.accessories.filter((item) => item.isOld))
+      sidebarStore.getFilters(tableStore.current)
+      tableStore.filtered = tableStore.current.rows
+    }
+  }
+})
 export default router
